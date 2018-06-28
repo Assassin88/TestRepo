a14c88de4c5c51e75f8d9a6478ee1b9df8bd1f8e
Paul <5540832@mail.ru>
2018-06-28 20:32:18 +0300

    Init2

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using EssentialTools.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products = {
+            new Product {Name = "Каяк", Category = "Водные виды спорта", Price = 275M},
+            new Product {Name = "Спасательный жилет", Category = "Водные виды спорта", Price = 48.95M},
+            new Product {Name = "Мяч", Category = "Футбол", Price = 19.50M},
+            new Product {Name = "Угловой флажок", Category = "Футбол", Price = 34.95M}
+        };
+
+
+        public ActionResult Index()
+        {
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+            ShoppingCart cart = new ShoppingCart(calc)
+            {
+                Products = products
+            };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
